8bbbff438413b9d286b281e0474177b8b2bffe6e
Nenad Mikša <nenad.miksa@microblink.com>
2023-04-06 16:58:42 +0200

    fix release build

--- Shared/Tunnel/BasePTProvider.swift
+++ Shared/Tunnel/BasePTProvider.swift
@@ -123,7 +123,7 @@ class BasePTProvider: NEPacketTunnelProvider {
                     IPtProxyStartSnowflake(
                         "stun:stun.l.google.com:19302,stun:stun.voip.blackberry.com:3478,stun:stun.altar.com.pl:3478,stun:stun.antisip.com:3478,stun:stun.bluesip.net:3478,stun:stun.dus.net:3478,stun:stun.epygi.com:3478,stun:stun.sonetel.com:3478,stun:stun.sonetel.net:3478,stun:stun.stunprotocol.org:3478,stun:stun.uls.co.za:3478,stun:stun.voipgate.com:3478,stun:stun.voys.nl:3478",
                         "https://snowflake-broker.torproject.net.global.prod.fastly.net/",
-                        "cdn.sstatic.net", nil, true, false, true, 1)
+                        "cdn.sstatic.net", nil, nil, true, false, true, 1)
 
                     port = IPtProxySnowflakePort()
 
